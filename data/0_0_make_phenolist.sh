089bf277724dcdb7bc5f330aef1fa5e6a29aeda2
Peter VandeHaar <pjvandehaar@gmail.com>
2016-10-13 23:27:12 -0400

    Configure Pheweb for Sardinia without new r^2

--- data/0_0_make_phenolist.sh
+++ data/0_0_make_phenolist.sh
@@ -5,7 +5,7 @@ PROJECT_DIR="$( cd "$( dirname "$( dirname "$(readlink -f "${BASH_SOURCE[0]}" )"
 source "$PROJECT_DIR/config.config"
 set -x
 
-./phenolist.py glob-files "/net/wonderland/home/csidore/Epacts_vcfs/Michelle_panel/GOODheader/R2sameasPapers/out-*/epacts-*.epacts.gz"
+./phenolist.py glob-files "/net/wonderland/home/csidore/Epacts_vcfs/Michelle_panel/GOODheader/out-*/epacts-*.epacts.gz"
 ./phenolist.py extract-phenocode-from-fname 'epacts-(.*)\.epacts\.gz'
 
 ./phenolist.py read-info-from-association-files
