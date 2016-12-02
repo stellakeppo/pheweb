54a1a1000a70cfa7eeab82169fc63963d8494e79
Peter VandeHaar <pjvandehaar@gmail.com>
2016-12-01 22:25:10 -0500

    Update for mgi-sna2

--- data/0_0_make_phenolist.sh
+++ data/0_0_make_phenolist.sh
@@ -5,7 +5,7 @@ PROJECT_DIR="$( cd "$( dirname "$( dirname "$(readlink -f "${BASH_SOURCE[0]}" )"
 source "$PROJECT_DIR/config.config"
 set -x
 
-./phenolist.py glob-files "/net/fantasia/home/schellen/PheWAS/epacts/RESULTS/pheno.*/pheno.*.epacts.gz" "/net/fantasia/home/schellen/PheWAS/epacts/RESULTS_chrX/pheno.*/pheno.*.epacts.gz"
+./phenolist.py glob-files "/net/fantasia/home/schellen/PheWAS/epacts/14NOV_SNA2_RESULTS/pheno.*/pheno.*.epacts.gz"
 ./phenolist.py extract-phenocode-from-fname 'pheno.([0-9\.]+).epacts.gz'
 ./phenolist.py unique-phenocode
 ./phenolist.py verify
@@ -15,7 +15,7 @@ set -x
 
 ./phenolist.py import-phenolist -f "$data_dir/pheno-list-phewascodes.json" "$PROJECT_DIR/unnecessary_things/PheWAS_code_translation_v1_2.txt"
 ./phenolist.py keep-only-columns -f "$data_dir/pheno-list-phewascodes.json" icd9_string category_string icd9 phenostring phenocode
-./phenolist.py rename-columns -f "$data_dir/pheno-list-phewascodes.json"   icd9 icd9_code   phenostring phenostring   phenocode phenocode   category_string category
+./phenolist.py rename-columns -f "$data_dir/pheno-list-phewascodes.json"   icd9 icd9_code   category_string category
 ./phenolist.py unique-phenocode -f "$data_dir/pheno-list-phewascodes.json" --columns-are-related icd9_info
 
 ./phenolist.py merge-in-info "$data_dir/pheno-list-phewascodes.json"
