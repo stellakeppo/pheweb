0a6c7d3ada145587ba11207c496ea7fd0640cbda
major seitan <majorsetian@gmail.com>
2021-10-06 19:17:44 +0000

    allow for missing ui config

--- pheweb/serve/components/config_ui/view.py
+++ pheweb/serve/components/config_ui/view.py
@@ -5,5 +5,9 @@ config_ui = Blueprint('config_ui', __name__)
 
 @config_ui.route('/api/config/ui', methods=["GET"])
 def get_config_ui():
-    app_dao = app.jeeves.config_ui
-    return json.dumps(app_dao.get_config())
+    if hasattr(app.jeeves, 'config_ui'):
+        app_dao = app.jeeves.config_ui
+        return json.dumps(app_dao.get_config())
+    else:
+        return json.dumps(None)
+    
