4e8b0667a3b9798c6ecf69d4c7bc3f75d2db3f27
major seitan <majorsetian@gmail.com>
2021-10-07 16:44:11 +0000

    fix the dao support

--- pheweb/serve/components/config_ui/view.py
+++ pheweb/serve/components/config_ui/view.py
@@ -5,9 +5,7 @@ config_ui = Blueprint('config_ui', __name__)
 
 @config_ui.route('/api/config/ui', methods=["GET"])
 def get_config_ui():
-    if hasattr(app.jeeves, 'config_ui'):
-        app_dao = app.jeeves.config_ui
-        return json.dumps(app_dao.get_config())
-    else:
-        return json.dumps(None)
+    app_dao = app.jeeves.config_ui
+    config_ui = app_dao if app_dao is None else app_dao.get_config_ui()
+    return json.dumps(config_ui)
     
