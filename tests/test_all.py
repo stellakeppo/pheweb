27167aa1ab590ccc02bad2347d58f23e07996aa2
major seitan <majorseitan@gmail.com>
2021-10-12 17:29:59 +0000

    format changes

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,41 +1,89 @@
-
-#TODO: use subprocess to eliminate conf_utils state?
-#TODO: split into multiple tests that share tmpdir and run in order
+# TODO: use subprocess to eliminate conf_utils state?
+# TODO: split into multiple tests that share tmpdir and run in order
 
 import os
 
+
 def test_all(tmpdir, capsys):
     p = tmpdir.realpath()
-    with capsys.disabled(): print('\n{}'.format(p))
-    indir = os.path.join(os.path.dirname(__file__), 'input_files/')
+    with capsys.disabled():
+        print("\n{}".format(p))
+    indir = os.path.join(os.path.dirname(__file__), "input_files/")
 
-    conf = ['conf',
-            'data_dir="{}"'.format(p),
-            'cache="{}/fake-cache"'.format(indir),
-            'browser="FINNGEN"',
-            'release=6',
-            'release_prev=5',
-            'logo=test.icon',
-            'title="testing"',
-            'page_title="hello world"',
-            'database_conf={ "annotation": { } }']
+    conf = [
+        "conf",
+        'data_dir="{}"'.format(p),
+        'cache="{}/fake-cache"'.format(indir),
+        'browser="FINNGEN"',
+        "release=6",
+        "release_prev=5",
+        "logo=test.icon",
+        'title="testing"',
+        'page_title="hello world"',
+        'database_conf={ "annotation": { } }',
+    ]
 
     from pheweb.command_line import run as cl_run
-    cl_run(conf+['-h'])
-    cl_run(conf+['phenolist', 'glob', '--simple-phenocode', '{}/assoc-files/*'.format(indir)])
-    cl_run(conf+['phenolist', 'unique-phenocode'])
+
+    cl_run(conf + ["-h"])
+    cl_run(
+        conf
+        + ["phenolist", "glob", "--simple-phenocode", "{}/assoc-files/*".format(indir)]
+    )
+    cl_run(conf + ["phenolist", "unique-phenocode"])
     # cl_run(conf+['phenolist', 'read-info-from-association-files'])
-    cl_run(conf+['phenolist', 'filter-phenotypes', '--minimum-num-cases', '20', '--minimum-num-controls', '20', '--minimum-num-samples', '20'])
-    cl_run(conf+['phenolist', 'hide-small-numbers-of-samples', '--minimum-visible-number', '50'])
-    cl_run(conf+['phenolist', 'hide-small-numbers-of-samples', '--minimum-visible-number', '50'])
-    cl_run(conf+['phenolist', 'import-phenolist', '-f', '{}/pheno-list-categories.json'.format(p), '{}/categories.csv'.format(indir)])
-    cl_run(conf+['phenolist', 'merge-in-info', '{}/pheno-list-categories.json'.format(p)])
-    cl_run(conf+['phenolist', 'verify', '--required-columns', 'category'])
-    with capsys.disabled(): print(1)
-    #cl_run(conf+['process'])
-    #cl_run(conf+['top-loci'])
-    cl_run(conf+['wsgi'])
-    with capsys.disabled(): print(2)
+    cl_run(
+        conf
+        + [
+            "phenolist",
+            "filter-phenotypes",
+            "--minimum-num-cases",
+            "20",
+            "--minimum-num-controls",
+            "20",
+            "--minimum-num-samples",
+            "20",
+        ]
+    )
+    cl_run(
+        conf
+        + [
+            "phenolist",
+            "hide-small-numbers-of-samples",
+            "--minimum-visible-number",
+            "50",
+        ]
+    )
+    cl_run(
+        conf
+        + [
+            "phenolist",
+            "hide-small-numbers-of-samples",
+            "--minimum-visible-number",
+            "50",
+        ]
+    )
+    cl_run(
+        conf
+        + [
+            "phenolist",
+            "import-phenolist",
+            "-f",
+            "{}/pheno-list-categories.json".format(p),
+            "{}/categories.csv".format(indir),
+        ]
+    )
+    cl_run(
+        conf + ["phenolist", "merge-in-info", "{}/pheno-list-categories.json".format(p)]
+    )
+    cl_run(conf + ["phenolist", "verify", "--required-columns", "category"])
+    with capsys.disabled():
+        print(1)
+    # cl_run(conf+['process'])
+    # cl_run(conf+['top-loci'])
+    cl_run(conf + ["wsgi"])
+    with capsys.disabled():
+        print(2)
 
     # from pheweb.serve.server import app # TODO: this relies on data_dir being set earlier, but shouldn't.
     # app.testing = True
